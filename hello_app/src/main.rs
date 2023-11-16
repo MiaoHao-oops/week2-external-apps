0dc14eac9cc063e87f7d6181d2806b7b2554b852
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-11-16 14:58:38 +0800

    feat: support cross elf abi calls from external app
    - add a linker script, and put `_start` to the beginning of the image
    - pack abi_calls to high level functions
    - IMPORTANT: inline asm should use `clobber_abi("C")`, thus tell the
      compiler to use callee saved registers
    - implement an api `puts(&str)`
    
    authored by: Miao Hao

--- hello_app/src/main.rs
+++ hello_app/src/main.rs
@@ -1,14 +1,78 @@
+#![feature(asm_const)]
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
+static HELLO_STRING: &str = "Hello from hello_app\n";
+static mut ABI_ENTRY: usize = 0;
+
+fn hello() {
+    unsafe {
+        core::arch::asm!("
+            li      a0, {abi_num}
+            la      t0, {abi_entry_addr}
+            ld      t0, (t0)
+            jalr    t0",
+            abi_num = const SYS_HELLO,
+            abi_entry_addr = sym ABI_ENTRY,
+            clobber_abi("C"),
+        )
+    }
+}
+
+fn putchar(c: u8) {
+    unsafe {
+        core::arch::asm!("
+            li      a0, {abi_num}
+            la      t0, {abi_entry_addr}
+            ld      t0, (t0)
+            jalr    t0",
+            abi_num = const SYS_PUTCHAR,
+            abi_entry_addr = sym ABI_ENTRY,
+            in("a1") c,
+            clobber_abi("C"),
+        )
+    }
+}
+
+fn terminate(exit_code: u8) {
+    unsafe {
+        core::arch::asm!("
+            li      a0, {abi_num}
+            la      t0, {abi_entry_addr}
+            ld      t0, (t0)
+            jalr    t0",
+            abi_num = const SYS_TERMINATE,
+            abi_entry_addr = sym ABI_ENTRY,
+            in("a1") exit_code,
+            clobber_abi("C"),
+        )
+    }
+}
+
+fn puts(s: &str) {
+    s.chars().for_each(|c| putchar(c as u8))
+}
 
 #[no_mangle]
-unsafe extern "C" fn _start() {
-    core::arch::asm!(
-        "nop",
-    );
+#[link_section = ".text.entry"]
+unsafe extern "C" fn _start(abi_entry: usize) -> ! {
+    ABI_ENTRY = abi_entry;
+    hello();
+    puts(HELLO_STRING);
+    terminate(0);
+
+    unsafe {
+        core::arch::asm!("
+            wfi",
+            options(noreturn),
+        )
+    }
 }
+
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
